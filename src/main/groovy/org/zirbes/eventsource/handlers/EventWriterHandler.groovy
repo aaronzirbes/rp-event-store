2e3fb5f8f77502de9546ba5663f853fe1c50045e
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2015-09-23 16:20:04 -0500

    adding test coverage

--- src/main/groovy/org/zirbes/eventsource/handlers/EventWriterHandler.groovy
+++ src/main/groovy/org/zirbes/eventsource/handlers/EventWriterHandler.groovy
@@ -9,11 +9,13 @@ import org.joda.time.LocalDateTime
 import com.thirdchannel.eventsource.AbstractEvent
 import org.zirbes.eventsource.services.EventLogWriter
 
+import ratpack.jackson.JsonRender
 import ratpack.groovy.handling.GroovyContext
 import ratpack.groovy.handling.GroovyHandler
 
 import static ratpack.jackson.Jackson.fromJson
 import static ratpack.jackson.Jackson.json
+
 
 @CompileStatic
 @Slf4j
@@ -22,34 +24,39 @@ class EventWriterHandler extends GroovyHandler {
     protected final HealthHandler health
     protected final EventLogWriter writer
 
+
     @Inject
     EventWriterHandler(EventLogWriter writer, HealthHandler health) {
         this.health = health
         this.writer = writer
     }
 
+
     @Override
     protected void handle(GroovyContext context) {
-        UUID key = UUID.randomUUID()
-        LocalDateTime now = LocalDateTime.now()
-
         context.render(
             context.parse(fromJson(AbstractEvent)).map{ AbstractEvent event ->
-                if (event?.data) {
-                    // Overwrite event ID / date
-                    event.id = key
-                    event.date = now.toDate()
-                    writer.writeEventAsync(event)
-                    log.info 'wrote event event.id={}', event.id
-                    // return response
-                    context.response.status(201)
-                    return json([key: key, date: now, message: 'queued'])
-                } else {
-                    context.response.status(204)
-                    return json([key: null, date: now, message: 'empty request'])
-                }
+                return handleEvent(event, context)
             }
         )
+    }
+
+    protected JsonRender handleEvent(AbstractEvent event, GroovyContext context) {
+        UUID key = UUID.randomUUID()
+        LocalDateTime now = LocalDateTime.now()
+        event.id = key
+        event.date = now.toDate()
+        if (event) {
+            // Overwrite event ID / date
+            writer.writeEventAsync(event)
+            log.info 'wrote event event.id={}', event.id
+            // return response
+            context.response.status(201)
+            return json([key: key, date: now, message: 'queued'])
+        } else {
+            context.response.status(204)
+            return json([key: null, date: now, message: 'empty request'])
+        }
     }
 
 }
